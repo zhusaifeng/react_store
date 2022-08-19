8cb1bf8070d4abc80cddf5a027db5f8e340f022e
zhusaifeng <2024274693@qq.com>
2022-08-19 17:07:21 +0800

    添加搜索和主页

--- src/components/SearchArea/index.js
+++ src/components/SearchArea/index.js
@@ -1,12 +1,102 @@
 import React, { Component } from 'react'
 import './index.css'
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
+import searchimg from './search.jpg'
 
 export default class index extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            success:false
+        }
+    }
     render() {
-        return (
-        <div>
-            <input  type="text" placeholder="请输入书名" className='search_input'></input>
-        </div>
+        console.log("执行render")
+        console.log("render中搜索到的书本信息",this)
+        // 需要读取bookid，查到书本才执行
+        if(this.state.success){
+            const bookid=this.state.getbook[0].bookId
+            console.log('bookid',bookid)
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input onChange={this.saveInput} type="text" name="userinput" className='search_input'/><br/>
+                        <Link to={'/BookDetail/'+bookid}><img className='searchimg' src={searchimg} alt="搜索图片"/></Link>
+                    </form>
+                </div>
+            )
+        }
+        else{
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input placeholder='点击回车搜索' onChange={this.saveInput} type="text" name="userinput" className='search_input'/><br/>
+                    </form>
+                </div>
+            )
+        }
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault()//阻止表单提交
+        const {userinput}=this.state
+        console.log("读取状态中的用户输入",userinput)
+        Axios.get('apibook/api-book-book-bynamelike/'+userinput).then(
+            response=>{
+                console.log(response.data)
+                this.setState({
+                    getbook:response.data,
+                })
+                // 如果返回的数据为空
+                if(response.data.length===0){
+                    this.setState({
+                        success:false
+                    })
+                    alert("暂无此书")
+                }
+                else{
+                    this.setState({
+                        success:true
+                    })
+                }
+            },
+            error=>{
+                console.log(error.message)
+                this.setState({
+                    success:false
+                })
+            }
+        )
+    }
+    saveInput=(e)=>{
+        console.log("保存用户输入的信息为：",e.target.value)
+        this.setState({
+            userinput:e.target.value
+        })//将用户输入写入状态,
+        Axios.get('apibook/api-book-book-bynamelike/'+e.target.value).then(
+            response=>{
+                console.log(response.data)
+                this.setState({
+                    getbook:response.data,
+                })
+                // 如果返回的数据为空
+                if(response.data.length===0){
+                    this.setState({
+                        success:false
+                    })
+                }
+                else{
+                    this.setState({
+                        success:true
+                    })
+                }
+            },
+            error=>{
+                console.log(error.message)
+                this.setState({
+                    success:false
+                })
+            }
         )
     }
 }
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import './index.css'
+import logo from './logo.ico'
 
 export default class index extends Component {
 render() {
     return (
-    <div>
-        我是User
+        <div class="container">
+        <div class="userinfo">
+            <div class="center-container">
+                <img src={logo} alt="logo" class="headphoto"/>
+                <div style={{textAlign:"center"}}>用户名:朱赛峰<br/>信用分:80分</div>
+            </div>
+        </div>
+        <div>
+            <div class="setting">当前借阅<div class="arrow"></div></div>
+            <div class="setting">借阅历史<div class="arrow"></div></div>
+            <div class="setting">账号设置<div class="arrow"></div></div>
+            <div class="setting">退出登录<div class="arrow"></div></div>
+        </div>
     </div>
     )
 }
